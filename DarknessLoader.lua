444fa7719e3327f8cb20f52964f11b99c7c1abb1
DarknessCFW <150121372+DarknessCFW@users.noreply.github.com>
2023-11-07 06:51:31 -0300

    Add files via upload

---
+++ DarknessLoader.lua
@@ -0,0 +1,20 @@
+if game.PlaceId == 14565278851 then
+    print("Anime Skill Fighting !")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/AnimeSkill.lua"))()
+elseif game.PlaceId == 15084364495 then
+    print("Bike Race Simulatort")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/Bicicleta.lua"))()
+elseif game.PlaceId == 14368340794 then
+    print("Swim Race Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/nadar.lua"))()
+elseif game.PlaceId == 9144187696 then
+    print("OP Magic Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/MagicBoxer.lua"))()
+elseif game.PlaceId == 14762790937 then
+    print("Anime Waves Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/Ondinha.lua"))()
+elseif game.PlaceId == 9144187696 then
+    print("Fighting Legends")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/Espadinha.lua"))()
+else
+end
