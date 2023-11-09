ddcc7f3d4ef8f495e94f3a2b21c8f0c419251928
DarknessCFW <150121372+DarknessCFW@users.noreply.github.com>
2023-11-09 11:09:18 -0300

    Update DarknessLoader.lua

--- DarknessLoader.lua
+++ DarknessLoader.lua
@@ -10,6 +10,9 @@ elseif game.PlaceId == 14368340794 then
 elseif game.PlaceId == 9144187696 then
     print("OP Magic Simulator")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/MagicBoxer.lua"))()
+elseif game.PlaceId == 15117193943 then
+    print("Snowboard Race Simulator")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/snowboard.lua"))()
 elseif game.PlaceId == 14762790937 then
     print("Anime Waves Simulator")
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DarknessCFW/DarknessOwned/main/Ondinha.lua"))()
